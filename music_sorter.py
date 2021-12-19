b4ca2072ab1ce9e9d59b8913d5909450215611e8
Dylan Barkowsky <dylanbarkowsky@gmail.com>
2021-12-18 16:33:54 -0800

    added class structure to sorting file

--- music_sorter.py
+++ music_sorter.py
@@ -4,43 +4,57 @@ import eyed3
 import os
 import shutil
 
-counter = 0
+
+
+class Music_Sorter:
+
+    #constructor
+    def __init__(self):
+        self.parent_dir = ""
+        self.counter = 0
+
+    def sort(self):
+        #loop through files
+        for path in os.listdir(self.parent_dir):
+            #if file and mp3
+            if os.path.isfile(path) and path.endswith(".mp3"):
+                #give file to eyeD3
+                file_name = eyed3.load(path)
+
+                try:
+                    #get artist name
+                    artist = file_name.tag.artist
+                    ######print(artist)
+                    
+                    #check if folder exists
+                    if os.path.isdir(artist):
+                        print(artist + " folder already exists")
+                        folder = os.path.join(self.parent_dir, artist)
+                    else:
+                        #create folder from artist name
+                        folder = os.path.join(self.parent_dir, artist)
+                        os.mkdir(folder)
+                        print("Creating " + folder)
+
+                    #copy file
+                    shutil.copy2(path,folder)
+                    print("Filed " + path + " successfully.")
+                    self.counter += 1
+
+                    #delete original file
+                    os.remove(path)
+                        
+                except :
+                    print("File \"" + path + "\" does not have artist information. No action taken.")
+
+        print("Moved " + str(self.counter) + " files into Artist folders.")
+
 
 #assume running from target folder
-parent_dir = os.getcwd()
-print("Running in " + parent_dir)
+#parent_dir = os.getcwd()
+#print("Running in " + parent_dir)
 
-#loop through files
-for path in os.listdir(parent_dir):
-    #if file and mp3
-    if os.path.isfile(path) and path.endswith(".mp3"):
-        #give file to eyeD3
-        file_name = eyed3.load(path)
-
-        try:
-            #get artist name
-            artist = file_name.tag.artist
-            ######print(artist)
-            
-            #check if folder exists
-            if os.path.isdir(artist):
-                print(artist + " folder already exists")
-                folder = os.path.join(parent_dir, artist)
-            else:
-                #create folder from artist name
-                folder = os.path.join(parent_dir, artist)
-                os.mkdir(folder)
-                print("Creating " + folder)
-
-            #copy file
-            shutil.copy2(path,folder)
-            print("Filed " + path + " successfully.")
-            counter += 1
-
-            #delete original file
-            os.remove(path)
-                
-        except :
-            print("File \"" + path + "\" does not have artist information. No action taken.")
-
-print("Moved " + str(counter) + " files into Artist folders.")
+#test cases
+test_sort = Music_Sorter()
+test_sort.parent_dir =  os.getcwd()
+test_sort.sort()
